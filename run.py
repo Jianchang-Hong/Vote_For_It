e6b8cfa4e348232d04b67db31ef2733372fc23aa
Jianchang-Hong-1161098 <hongjianchang@163.com>
2025-06-11 21:53:47 +1200

    update log

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from voting import app
 
 if __name__ == "__main__":
     '''Enables debug mode and sets the server to run on port 80, which is the default HTTP port'''
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
